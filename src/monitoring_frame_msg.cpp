2547e4eef57fac1e4ed16654b8260b26b00b6854
Alexander Gutenkunst <a.gutenkunst@pilz.de>
2020-09-29 12:37:48 +0200

    Improve from raw_data

--- src/monitoring_frame_msg.cpp
+++ src/monitoring_frame_msg.cpp
@@ -57,13 +57,8 @@ MonitoringFrameMsg MonitoringFrameMsg::fromRawData(const MaxSizeRawData& data)
   raw_processing::read(is, msg.transaction_type_fixed_);
   raw_processing::read(is, msg.scanner_id_fixed_);
 
-  uint16_t raw_value_from_theta;
-  raw_processing::read(is, raw_value_from_theta);
-  msg.from_theta_fixed_ = TenthOfDegree(raw_value_from_theta);
-
-  uint16_t raw_value_resolution;
-  raw_processing::read(is, raw_value_resolution);
-  msg.resolution_fixed_ = TenthOfDegree(raw_value_resolution);
+  raw_processing::read<uint16_t, TenthOfDegree>(is, msg.from_theta_fixed_);
+  raw_processing::read<uint16_t, TenthOfDegree>(is, msg.resolution_fixed_);
 
   msg.checkFixedFields();
 
