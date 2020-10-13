7d232c3046b51b2369b7c35e20d2ad6db7b34f88
Immanuel Martini <i.martini@pilz.de>
2020-10-13 14:49:52 +0200

    Add macro for throttled log output

--- include/psen_scan_v2/logging.h
+++ include/psen_scan_v2/logging.h
@@ -16,6 +16,7 @@
 #ifndef PSEN_SCAN_V2_LOGGING_H
 #define PSEN_SCAN_V2_LOGGING_H
 
+#include <chrono>
 #include <console_bridge/console.h>
 #include <sstream>
 #include <fmt/core.h>
@@ -25,11 +26,31 @@
     console_bridge::getOutputHandler()->log(fmt::format("{}: {}", name, fmt::format(__VA_ARGS__)), level, file, line); \
   }
 
+#define PSENSCAN_LOG_THROTTLE(period, name, file, line, level, ...)                                                    \
+  {                                                                                                                    \
+    static std::chrono::system_clock::time_point throttle_last_hit;                                                    \
+    auto throttle_now = std::chrono::system_clock::now();                                                              \
+    if (throttle_last_hit + std::chrono::duration<double>(period) < throttle_now)                                      \
+    {                                                                                                                  \
+      throttle_last_hit = throttle_now;                                                                                \
+      PSENSCAN_LOG(name, file, line, level, __VA_ARGS__);                                                              \
+    }                                                                                                                  \
+  }
+
 using namespace console_bridge;
 
 #define PSENSCAN_ERROR(name, ...) PSENSCAN_LOG(name, __FILE__, __LINE__, CONSOLE_BRIDGE_LOG_ERROR, __VA_ARGS__)
 #define PSENSCAN_INFO(name, ...) PSENSCAN_LOG(name, __FILE__, __LINE__, CONSOLE_BRIDGE_LOG_INFO, __VA_ARGS__)
 #define PSENSCAN_WARN(name, ...) PSENSCAN_LOG(name, __FILE__, __LINE__, CONSOLE_BRIDGE_LOG_WARN, __VA_ARGS__)
 #define PSENSCAN_DEBUG(name, ...) PSENSCAN_LOG(name, __FILE__, __LINE__, CONSOLE_BRIDGE_LOG_DEBUG, __VA_ARGS__)
+
+#define PSENSCAN_ERROR_THROTTLE(period, name, ...)                                                                     \
+  PSENSCAN_LOG_THROTTLE(period, name, __FILE__, __LINE__, CONSOLE_BRIDGE_LOG_ERROR, __VA_ARGS__)
+#define PSENSCAN_INFO_THROTTLE(period, name, ...)                                                                      \
+  PSENSCAN_LOG_THROTTLE(period, name, __FILE__, __LINE__, CONSOLE_BRIDGE_LOG_INFO, __VA_ARGS__)
+#define PSENSCAN_WARN_THROTTLE(period, name, ...)                                                                      \
+  PSENSCAN_LOG_THROTTLE(period, name, __FILE__, __LINE__, CONSOLE_BRIDGE_LOG_WARN, __VA_ARGS__)
+#define PSENSCAN_DEBUG_THROTTLE(period, name, ...)                                                                     \
+  PSENSCAN_LOG_THROTTLE(period, name, __FILE__, __LINE__, CONSOLE_BRIDGE_LOG_DEBUG, __VA_ARGS__)
 
 #endif  // PSEN_SCAN_V2_LOGGING_H
