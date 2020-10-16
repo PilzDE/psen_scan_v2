9e2ba7df6ea9c6219212c1245e7d9a4a8f7d2aa6
Christian Döhn <c.doehn@pilz.de>
2020-10-16 15:21:42 +0200

    Refactor valid hex dump

--- test/include/psen_scan_v2/udp_frame_dumps.h
+++ test/include/psen_scan_v2/udp_frame_dumps.h
@@ -27,24 +27,35 @@ using namespace psen_scan_v2;
 
 namespace psen_scan_v2_test
 {
-struct UDPFrameTestDataWithoutIntensities
+template <size_t ARRAY_SIZE>
+inline std::vector<double> readMeasuresFromHexDump(const std::array<uint8_t, ARRAY_SIZE> hex_dump,
+                                                   const size_t offset_measures,
+                                                   const size_t n_measures)
 {
+  std::vector<double> measures;
+  for (size_t idx = offset_measures; idx < (offset_measures + (n_measures * 2)); idx = idx + 2)
+  {
+    uint16_t raw_value = (((uint16_t)hex_dump.at(idx + 1)) << 8) + (uint16_t)hex_dump.at(idx);
+    measures.push_back(raw_value / 1000.);
+  }
+  return measures;
+}
+
+class UDPFrameTestDataWithoutIntensities
+{
+public:
   UDPFrameTestDataWithoutIntensities()
   {
-    std::vector<double> measures;
-
-    for (size_t idx = offset_measures; idx < (offset_measures + (n_measures * 2)); idx = idx + 2)
-    {
-      uint16_t raw_value = (((uint16_t)hex_dump.at(idx + 1)) << 8) + (uint16_t)hex_dump.at(idx);
-      measures.push_back(raw_value / 1000.);
-    }
-
-    MonitoringFrameMsg msg(TenthOfDegree(from_theta), TenthOfDegree(resolution), scan_counter, measures);
+    MonitoringFrameMsg msg(TenthOfDegree(from_theta),
+                           TenthOfDegree(resolution),
+                           scan_counter,
+                           readMeasuresFromHexDump(hex_dump, offset_measures, n_measures));
     msg_ = msg;
-  }
+  };
 
+  // clang-format off
   const std::array<uint8_t, 135> hex_dump = {
-    0x00, 0x00, 0x00, 0x00, 0xca, 0x00,                                                              // 0020
+                                                                0x00, 0x00, 0x00, 0x00, 0xca, 0x00,  // 0020
     0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x05, 0x00, 0x00, 0x00, 0x00, 0xe8, 0x03, 0x0a, 0x00, 0x02,  // 0030
     0x05, 0x00, 0x78, 0x06, 0x00, 0x00, 0x05, 0x65, 0x00, 0xa9, 0x02, 0x06, 0x03, 0x7c, 0x02, 0xfa,  // 0040
     0x01, 0xf0, 0x01, 0x0e, 0x02, 0x77, 0x01, 0xb4, 0x01, 0xd6, 0x01, 0xd8, 0x01, 0xe5, 0x01, 0xe1,  // 0050
@@ -54,65 +65,71 @@ struct UDPFrameTestDataWithoutIntensities
     0x09, 0x0e, 0x09, 0xe7, 0x08, 0xe7, 0x08, 0xdd, 0x08, 0xeb, 0x08, 0xeb, 0x08, 0xd4, 0x08, 0xf1,  // 0090
     0x08, 0x04, 0x09, 0xea, 0x0f, 0xea, 0x0f, 0x03, 0x10, 0xc6, 0x10, 0x31, 0x12, 0x09, 0x00, 0x00,  // 00a0
     0x00                                                                                             // 00b0
-  };  // hex dump: successful_stop_request.pcapng md5 e800c4 No. 22 only data payload of the udp frame
+  };
+  // clang-format on
 
-  const int n_measures{ (hex_dump.at(30) * 0xff + hex_dump.at(29) - 1) / 2 };
+  MonitoringFrameMsg msg_;
+
+private:
+  const int n_measures{ 50 };
   const size_t offset_measures{ 31 };
 
   const uint16_t from_theta{ 0x03e8 };
   const uint16_t resolution{ 0x0a };
   const uint32_t scan_counter{ 0x0678 };
-
-  const uint16_t number_of_measures{ 50 };
-
-  MonitoringFrameMsg msg_;
 };
 
-struct UDPFrameTestDataWithoutMeasurementsAndIntensities
+class UDPFrameTestDataWithoutMeasurementsAndIntensities
 {
-  MonitoringFrameMsg msg_;
-
+public:
   UDPFrameTestDataWithoutMeasurementsAndIntensities()
   {
     MonitoringFrameMsg msg(TenthOfDegree(from_theta), TenthOfDegree(resolution), scan_counter, {});
     msg_ = msg;
   }
 
-  const uint16_t from_theta{ 0x5dc };
-  const uint16_t resolution{ 0x0a };
-  const uint32_t scan_counter{ 0x0661fc };
-
   const std::array<uint8_t, 35> hex_dump = { 0x00, 0x00, 0x00, 0x00, 0xca, 0x00,  // 0020
                                              0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x05, 0x00, 0x00, 0x00,
                                              0x00, 0xdc, 0x05, 0x0a, 0x00, 0x02, 0x05, 0x00, 0xfc, 0x61,
                                              0x06, 0x00, 0x05, 0x01, 0x00, 0x09, 0x00, 0x00, 0x00 };
+
+  MonitoringFrameMsg msg_;
+
+private:
+  const uint16_t from_theta{ 0x5dc };
+  const uint16_t resolution{ 0x0a };
+  const uint32_t scan_counter{ 0x0661fc };
 };
 
-struct UDPFrameTestDataWithUnknownFieldId
+class UDPFrameTestDataWithUnknownFieldId
 {
+public:
   const std::array<uint8_t, 35> hex_dump = { 0x00, 0x00, 0x00, 0x00, 0xca, 0x00,  // 0020
                                              0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x05, 0x00, 0x00, 0x00,
                                              0x00, 0xdc, 0x05, 0x0a, 0x00, 0x02, 0x05, 0x00, 0xfc, 0x61,
                                              0x06, 0x00, 0x0a, 0x01, 0x00, 0x09, 0x00, 0x00, 0x00 };
 };
 
-struct UDPFrameTestDataWithTooLargeFieldLength
+class UDPFrameTestDataWithTooLargeFieldLength
 {
+public:
   const std::array<uint8_t, 35> hex_dump = { 0x00, 0x00, 0x00, 0x00, 0xca, 0x00,  // 0020
                                              0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x05, 0x00, 0x00, 0x00,
                                              0x00, 0xdc, 0x05, 0x0a, 0x00, 0x02, 0x05, 0x00, 0xfc, 0x61,
                                              0x06, 0x00, 0x05, 0xcf, 0xff, 0x09, 0x00, 0x00, 0x00 };
 };
 
-struct UDPFrameTestDataWithTooLargeScanCounterLength
+class UDPFrameTestDataWithTooLargeScanCounterLength
 {
-  unsigned char modv = 0x06;
+public:
   const std::array<uint8_t, 35> hex_dump = { 0x00, 0x00, 0x00, 0x00, 0xca, 0x00,  // 0020
                                              0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x05, 0x00, 0x00, 0x00,
                                              0x00, 0xdc, 0x05, 0x0a, 0x00, 0x02, modv, 0x00, 0xfc, 0x61,
                                              0x06, 0x00, 0x05, 0xcf, 0xff, 0x09, 0x00, 0x00, 0x00 };
-};
 
+private:
+  unsigned char modv = 0x06;
+};
 }  // namespace psen_scan_v2_test
 
 #endif  // PSEN_SCAN_V2_UDP_FRAME_DUMPS_H
