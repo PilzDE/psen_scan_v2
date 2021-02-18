668367277675ef7dcb98f2d1c63feebff65de17d
Immanuel Martini <i.martini@pilz.de>
2021-02-18 12:03:54 +0100

    Fix automatic linking in doxygen docu

--- standalone/include/psen_scan_v2_standalone/scanner_v2.h
+++ standalone/include/psen_scan_v2_standalone/scanner_v2.h
@@ -43,14 +43,14 @@ using std::placeholders::_2;
  * @brief This is the API implementation for external interaction with the PSENscan driver.
  *
  * This class is responsible for the initialization of:
- * - the ScannerStateMachine.
+ * - the state machine.
  * - the Udp connections.
  * - the guards to ensure threads save interaction between the user, udp connections and timeouts.
  *
  * It uses the passed ScannerConfiguration for all configurable parts of this process.
  *
  * @see IScanner
- * @see ScannerStateMachine
+ * @see scanner_protocol::ScannerStateMachine
  * @see ScannerConfiguration
  */
 class ScannerV2 : public IScanner
