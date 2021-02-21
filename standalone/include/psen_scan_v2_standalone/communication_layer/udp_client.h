0781b0cbdb8a3b79e9f817e2325e7c26833738ec
Alexander Gutenkunst <a.gutenkunst@pilz.de>
2021-02-21 22:21:10 +0100

    Remove unused boost includes (#153)

--- standalone/include/psen_scan_v2_standalone/communication_layer/udp_client.h
+++ standalone/include/psen_scan_v2_standalone/communication_layer/udp_client.h
@@ -33,7 +33,6 @@
 #endif
 
 #include <boost/asio.hpp>
-#include <boost/asio/high_resolution_timer.hpp>
 #include <boost/bind.hpp>
 
 #include "psen_scan_v2_standalone/data_conversion_layer/raw_scanner_data.h"
