646763f03020a38cfb659d248c6b49d48813eb34
Christian Döhn <c.doehn@pilz.de>
2021-06-11 13:58:49 +0200

    Incorporate review comments

--- test/hw_tests/hwtest_scan_range.py
+++ test/hw_tests/hwtest_scan_range.py
@@ -7,12 +7,13 @@ from math import degrees
 import rospy
 from sensor_msgs.msg import LaserScan
 
-import pytest
 
-PLACES = 6
+DECIMAL_PLACE_ACCURACY = 6
 
 
 class HwtestScanRange(unittest.TestCase):
+    """ Check if the laserscan message covers the scan range as expected.
+    """
     def setUp(self):
         rospy.init_node('hwtest_scan_range')
         self.received_msgs = []
@@ -29,24 +30,14 @@ class HwtestScanRange(unittest.TestCase):
         self.assertGreaterEqual(len(self.received_msgs), 1)
 
         message: LaserScan = self.received_msgs[0]
-        rospy.loginfo(f"param angle_start:\n {degrees(self.angle_start)}")
-        rospy.loginfo(f"param angle_end:\n {degrees(self.angle_end)}")
-        rospy.loginfo(f"msg angle_min:\n {degrees(message.angle_min)}")
-        rospy.loginfo(f"msg angle_max:\n {degrees(message.angle_max)}")
-        rospy.loginfo(
-            f"msg angle_increment:\n {degrees(message.angle_increment)}")
-        rospy.loginfo("msg angle_increment * len(ranges):\n" +
-                      f" {degrees(message.angle_increment * len(message.ranges))}")
-        rospy.loginfo("msg angle_increment * (len(ranges)-1):\n" +
-                      f" {degrees(message.angle_increment * (len(message.ranges)-1))}")
-        rospy.loginfo("msg angle_min + angle_increment * (len(ranges)-1):\n" +
-                      f" {degrees(message.angle_min + message.angle_increment * (len(message.ranges)-1))}")
 
-        self.assertAlmostEqual(self.angle_start, message.angle_min, PLACES)
-        self.assertAlmostEqual(self.angle_end,
-                               message.angle_min + message.angle_increment *
-                               (len(message.ranges)-1),
-                               PLACES)
+        self.assertAlmostEqual(self.angle_start, message.angle_min, DECIMAL_PLACE_ACCURACY,
+                               "angle_min of the laserscan message is " + str(message.angle_min) +
+                               " but should be " + str(self.angle_start) + ".")
+        angle_max = message.angle_min + message.angle_increment * (len(message.ranges)-1)
+        self.assertAlmostEqual(self.angle_end, angle_max, DECIMAL_PLACE_ACCURACY,
+                               "angle_max of the laserscan message is " + str(angle_max) +
+                               " but should be " + str(self.angle_end) + ".")
 
 
 if __name__ == '__main__':
