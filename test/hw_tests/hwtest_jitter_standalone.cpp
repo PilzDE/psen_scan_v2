33c1ba6688ed441801c83010f8d14ce301e0b856
Immanuel Martini <i.martini@pilz.de>
2021-08-27 15:42:37 +0200

    Add missing includes

--- test/hw_tests/hwtest_jitter_standalone.cpp
+++ test/hw_tests/hwtest_jitter_standalone.cpp
@@ -13,6 +13,9 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+#include <cstdlib>
+#include <functional>
+
 #include <gtest/gtest.h>
 
 #include "psen_scan_v2_standalone/core.h"
