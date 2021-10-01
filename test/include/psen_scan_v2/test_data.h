09a59997f33a3488b1cd1a9a0eec1743ade791e6
Immanuel Martini <35950815+martiniil@users.noreply.github.com>
2021-10-01 10:32:29 +0200

    Automate timestamp hardware-test (#231)
    * Automate timestamp hardware-test
    * Add CMake argument ENABLE_HARDWARE_TESTING_WITH_WIRESHARK
    * Add cpp unit-test hwtest_timestamp_standalone
    * Add TestData, UdpDataReader and TestDataAssembler
    * Add sh-script for recording udp packets
    * Document test procedure in test/hw_tests/hwtest_readme.md

---
+++ test/include/psen_scan_v2/test_data.h
@@ -0,0 +1,57 @@
+// Copyright (c) 2021 Pilz GmbH & Co. KG
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+#ifndef PSEN_SCAN_V2_TEST_TEST_DATA_H
+#define PSEN_SCAN_V2_TEST_TEST_DATA_H
+
+#include <cstdint>
+#include <vector>
+
+#include <boost/optional.hpp>
+
+namespace psen_scan_v2_test
+{
+namespace test_data
+{
+class TestDatum
+{
+public:
+  TestDatum(const uint32_t scan_counter, const int64_t timestamp, const int64_t callback_invocation_time);
+
+  void setFirstFrameTime(const int64_t first_frame_time);
+  void setLastFrameTime(const int64_t last_frame_time);
+
+  bool isComplete() const;
+
+  uint32_t scanCounter() const;
+  int64_t timestamp() const;
+  int64_t callbackInvocationTime() const;
+  int64_t firstFrameTime() const;
+  int64_t lastFrameTime() const;
+
+private:
+  uint32_t scan_counter_;
+  int64_t timestamp_;
+  int64_t callback_invocation_time_;
+  boost::optional<int64_t> first_frame_time_;
+  boost::optional<int64_t> last_frame_time_;
+};
+
+using TestData = std::vector<TestDatum>;
+
+}  // namespace test_data
+}  // namespace psen_scan_v2_test
+
+#endif  // PSEN_SCAN_V2_TEST_TEST_DATA_H
