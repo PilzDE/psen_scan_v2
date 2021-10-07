aed4a2ff966bfc31d768dbbaf78a577582eb1edd
rfeistenauer <45563842+rfeistenauer@users.noreply.github.com>
2021-10-07 05:55:42 +0200

    Update standalone/test/integration_tests/api/integrationtest_scanner_api.cpp
    Co-authored-by: Alexander Gutenkunst <a.gutenkunst@pilz.de>

--- standalone/test/integration_tests/api/integrationtest_scanner_api.cpp
+++ standalone/test/integration_tests/api/integrationtest_scanner_api.cpp
@@ -116,7 +116,7 @@ public:
     EXPECT_STOP_REQUEST_CALL(*hw_mock).WillOnce(OpenBarrier(&stop_req_barrier));                                       \
     EXPECT_DOES_NOT_BLOCK(stop_future = driver->stop(););                                                              \
     EXPECT_BARRIER_OPENS(stop_req_barrier, DEFAULT_TIMEOUT) << "Stop request not received";                            \
-    hw_mock_->sendStopReply();                                                                                         \
+    hw_mock->sendStopReply();                                                                                         \
     EXPECT_FUTURE_IS_READY(stop_future) << "Scanner::stop() not finished";                                             \
   } while (false)
 
