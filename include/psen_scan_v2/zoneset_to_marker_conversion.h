86628e81d5d9edecd3cc804c29caacedcd0f1472
Immanuel Martini <i.martini@pilz.de>
2021-10-29 17:42:58 +0200

    Add zoneset-to-marker logic

--- include/psen_scan_v2/zoneset_to_marker_conversion.h
+++ include/psen_scan_v2/zoneset_to_marker_conversion.h
@@ -16,18 +16,131 @@
 #ifndef PSEN_SCAN_V2_ZONESET_TO_MARKER_CONVERSION_H
 #define PSEN_SCAN_V2_ZONESET_TO_MARKER_CONVERSION_H
 
+#include <string>
 #include <vector>
 
+#include <fmt/format.h>
+
+#include <geometry_msgs/Point.h>
+#include <geometry_msgs/Point32.h>
+#include <std_msgs/ColorRGBA.h>
 #include <visualization_msgs/Marker.h>
 
 #include "psen_scan_v2/ZoneSet.h"
 
+#define TO_MARKER(zoneset_obj, polygon_type, polygon_index)                                                            \
+  createMarker(fmt::format("active zoneset {}{} {}", #polygon_type, #polygon_index, getRangeInfo(zoneset_obj)),        \
+               strcmp(#polygon_type, "safety") == 0,                                                                   \
+               createRGBA(strcmp(#polygon_type, "muting") == 0,                                                        \
+                          strcmp(#polygon_type, "warn") == 0,                                                          \
+                          strcmp(#polygon_type, "muting") == 0,                                                        \
+                          1),                                                                                          \
+               zoneset_obj.header.frame_id,                                                                            \
+               zoneset_obj.polygon_type##polygon_index.points,                                                         \
+               0.01 * ((strcmp(#polygon_type, "warn") == 0) + (strcmp(#polygon_type, "muting") == 0)))
+
 namespace psen_scan_v2
 {
-std::vector<visualization_msgs::Marker> toMarkers(const ZoneSet& zoneset_config)
+geometry_msgs::Point createPoint(const double& x, const double& y, const double& z)
 {
-  // ToDo
-  return { visualization_msgs::Marker() };
+  geometry_msgs::Point p;
+  p.x = x;
+  p.y = y;
+  p.z = z;
+  return p;
+}
+
+std_msgs::ColorRGBA createRGBA(const float& r, const float& g, const float& b, const float& a)
+{
+  std_msgs::ColorRGBA color;
+  color.r = r;
+  color.g = g;
+  color.b = b;
+  color.a = a;
+  return color;
+}
+
+visualization_msgs::Marker createMarker(const std::string& ns,
+                                        int32_t id,
+                                        const std_msgs::ColorRGBA& color,
+                                        const std::string& frame_id,
+                                        const std::vector<geometry_msgs::Point32>& points,
+                                        const double& z_offset = 0)
+{
+  auto marker = visualization_msgs::Marker();
+  marker.header.frame_id = frame_id;
+  marker.ns = ns;
+  marker.id = id;
+  marker.type = marker.TRIANGLE_LIST;
+  marker.action = marker.ADD;
+  marker.scale.x = 1.0;
+  marker.scale.y = 1.0;
+  marker.scale.z = 1.0;
+  marker.color.a = 0.4;
+
+  marker.pose.orientation.z = 0.0;
+  marker.pose.orientation.w = 1.0;
+  marker.pose.position.x = 0.0;
+  marker.pose.position.y = 0.0;
+  marker.pose.position.z = z_offset;
+
+  marker.points.reserve(3 * (points.size() - 1));
+  marker.colors.reserve(points.size() - 1);
+
+  for (std::size_t i = 1; i < points.size(); ++i)
+  {
+    marker.points.push_back(createPoint(0.0, 0.0, 0.0));
+    marker.points.push_back(createPoint(points.at(i - 1).x, points.at(i - 1).y, 0.0));
+    marker.points.push_back(createPoint(points.at(i).x, points.at(i).y, 0.0));
+    marker.colors.push_back(color);
+  }
+  return marker;
+}
+
+std::string getRangeInfo(const ZoneSet& zoneset)
+{
+  std::string range_info = "";
+  if (zoneset.speed_lower != 0 || zoneset.speed_upper != 0)
+  {
+    range_info = fmt::format("min:{:+} max:{:+}", zoneset.speed_lower, zoneset.speed_upper);
+  }
+  return range_info;
+}
+
+std::vector<visualization_msgs::Marker> toMarkers(const ZoneSet& zoneset)
+{
+  std::vector<visualization_msgs::Marker> return_vec;
+
+  if (!zoneset.safety1.points.empty())
+  {
+    return_vec.push_back(TO_MARKER(zoneset, safety, 1));
+  }
+  if (!zoneset.safety2.points.empty())
+  {
+    return_vec.push_back(TO_MARKER(zoneset, safety, 2));
+  }
+  if (!zoneset.safety3.points.empty())
+  {
+    return_vec.push_back(TO_MARKER(zoneset, safety, 3));
+  }
+  if (!zoneset.warn1.points.empty())
+  {
+    return_vec.push_back(TO_MARKER(zoneset, warn, 1));
+  }
+  if (!zoneset.warn2.points.empty())
+  {
+    return_vec.push_back(TO_MARKER(zoneset, warn, 2));
+  }
+  if (!zoneset.muting1.points.empty())
+  {
+    return_vec.push_back(TO_MARKER(zoneset, muting, 1));
+  }
+  if (!zoneset.muting2.points.empty())
+  {
+    return_vec.push_back(TO_MARKER(zoneset, muting, 2));
+  }
+
+  return return_vec;
 }
 
 }  // namespace psen_scan_v2
