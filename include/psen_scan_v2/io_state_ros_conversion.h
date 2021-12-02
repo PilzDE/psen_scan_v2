edfd2fb3923e03a158f5a5ca8c5023ffa801c4ed
Richard Feistenauer <r.feistenauer@pilz.de>
2021-12-02 11:00:47 +0100

    Add conversion and publish of io_state

---
+++ include/psen_scan_v2/io_state_ros_conversion.h
@@ -0,0 +1,66 @@
+// Copyright (c) 2021 Pilz GmbH & Co. KG
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+#ifndef PSEN_SCAN_V2_IO_STATE_ROS_CONVERSIONS_H
+#define PSEN_SCAN_V2_IO_STATE_ROS_CONVERSIONS_H
+
+#include <string>
+
+#include <psen_scan_v2/IOState.h>
+#include <psen_scan_v2/PinState.h>
+#include <psen_scan_v2_standalone/io_state.h>
+
+namespace psen_scan_v2
+{
+psen_scan_v2::PinState toPinStateMsg(const psen_scan_v2_standalone::PinState& pin)
+{
+  psen_scan_v2::PinState pin_msg;
+  pin_msg.pin_id = pin.id();
+  pin_msg.name = pin.name();
+  pin_msg.state = pin.state();
+  return pin_msg;
+}
+
+void readPinStates(std::vector<psen_scan_v2::PinState>& container,
+                   const std::vector<psen_scan_v2_standalone::PinState>& pins)
+{
+  for (const auto& pin : pins)
+  {
+    container.push_back(toPinStateMsg(pin));
+  }
+}
+
+psen_scan_v2::IOState toIOStateMsg(const psen_scan_v2_standalone::IOState& io_state,
+                                   std::string frame_id,
+                                   int64_t stamp)
+{
+  psen_scan_v2::IOState ros_message;
+  if (stamp < 0)
+  {
+    throw std::invalid_argument("Laserscan message has an invalid timestamp: " + std::to_string(stamp));
+  }
+  ros_message.header.stamp = ros::Time{}.fromNSec(stamp);
+  ros_message.header.frame_id = frame_id;
+
+  readPinStates(ros_message.physical_input, io_state.physicalInput());
+  readPinStates(ros_message.logical_input, io_state.logicalInput());
+  readPinStates(ros_message.output, io_state.output());
+
+  return ros_message;
+}
+
+}  // namespace psen_scan_v2
+
+#endif  // PSEN_SCAN_V2_IO_STATE_ROS_CONVERSIONS_H
