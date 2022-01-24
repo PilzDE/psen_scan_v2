dfe1fdd7dbbb22b380c434fc4439992b922efcc8
Richard Feistenauer <r.feistenauer@pilz.de>
2022-01-24 10:23:56 +0100

    Fix clang

--- src/config_server_node.cpp
+++ src/config_server_node.cpp
@@ -35,7 +35,8 @@ ConfigServerNode::ConfigServerNode(ros::NodeHandle& nh, const char* config_file_
     ROS_WARN_STREAM_NAMED(
         "ConfigurationServer",
         "The configuration server doesn't verfiy the provided config mathes the one on the connected hardware! "
-        "You are using \"" + std::string(config_file_path) + "\" please verfiy its validity.");
+        "You are using \"" +
+            std::string(config_file_path) + "\" please verfiy its validity.");
 
     zoneset_pub_.publish(toRosMsg(zoneconfig, frame_id));
   }
